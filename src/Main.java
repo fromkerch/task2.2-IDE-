74b6b96edc2a4d1a924fd68f2997a7432c3d2155
Pavel Smirnov <luq2009@yandex.ru>
2020-11-21 14:43:11 +0300

    added gitignore and README files

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+         float regularBonus = (float) 0.3;
+         float specialBonus = (float) 0.6;
+         float totalBonus = regularBonus + specialBonus;
+        System.out.println(totalBonus);
+    }
+}
